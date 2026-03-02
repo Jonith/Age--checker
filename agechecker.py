4a008d777f7a23079b3b5fe693568236473da0f9
Jonith <jexon.joseph@yahoo.com>
2026-03-02 13:08:35 +0530

    Intial Commit

--- agechecker.py
+++ agechecker.py
@@ -1,7 +1,8 @@
-age=int(intput("Enter your age"))
-if age<10: 
-    print("under 10")
-    if age<20:
-        print("under 20")
+age=int(input("Enter your age:"))
+if age<20: 
+    print("under 20")
+    
+elif age>20:
+    print("above 20")
 else:
     print("Invalid Choice")
